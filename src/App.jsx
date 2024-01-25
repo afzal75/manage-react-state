95f1ba955c295f7cad0e01c31c316245f814224c
afzal75 <afzalbhuiyan16@gmail.com>
2024-01-25 11:30:49 +0600

    redudent state

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Form from "./components/Form";
+import Forms from "./components/Forms";
+
+function App() {
+  return (
+    <div>
+      <Forms />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Forms.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+export default function Forms() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  // const [fullName, setFullName] = useState("");
+
+  function handleFirstNameChange(e) {
+    setFirstName(e.target.value);
+  }
+
+  function handleLastNameChange(e) {
+    setLastName(e.target.value);
+  }
+
+  // derived state or calculated state
+
+  const fullName = firstName + " " + lastName;
+
+  return (
+    <>
+      <h2>Let’s check you in</h2>
+      <label>
+        First name: <input value={firstName} onChange={handleFirstNameChange} />
+      </label>
+      <label>
+        Last name: <input value={lastName} onChange={handleLastNameChange} />
+      </label>
+      <p>
+        Your ticket will be issued to: <b>{fullName}</b>
+      </p>
+    </>
+  );
+}
